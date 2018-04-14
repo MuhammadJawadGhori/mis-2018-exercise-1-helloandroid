0bbc4e3020710c4be24e343f982d220e931050d6
Jawad <jawadghori33@gmail.com>
2018-04-15 05:44:15 +1300

    Made changes to add webview so when the user enters url, it gets displayed in the webview.

--- app/src/main/java/com/example/mis/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/mis/helloandroid/MainActivity.java
@@ -1,23 +1,68 @@
 package com.example.mis.helloandroid;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    WebView webView;
+    WebViewClient webViewClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+
+        webView = findViewById(R.id.webViewId);
+        //webView.getSettings().setJavaScriptEnabled(true);
     }
 
-    public void helloWorld(View view) {
-        Toast toast = Toast.makeText(MainActivity.this, "hey there", Toast.LENGTH_SHORT);
-        toast.setGravity(Gravity.CENTER, 0 ,0);
-        toast.show();
+    public void getUrl(View view){
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+
+        webView.loadUrl(editText.getText().toString());
+        webViewClient = new WebViewClient(){
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                System.out.println("On page finished");
+            }
+
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                Toast.makeText(getApplicationContext(), "Your Internet Connection May not be active Or " + error , Toast.LENGTH_LONG).show();
+                super.onReceivedError(view, request, error);
+            }
+        };
+
+        webView.setWebViewClient(webViewClient);
+
     }
+
+
 }
